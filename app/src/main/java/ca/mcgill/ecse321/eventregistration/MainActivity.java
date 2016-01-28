1bf525a1d9faa7059b3e154f34d8d57e4ddce08e
kareemhalabi <kareemhalabi24@gmail.com>
2016-01-27 20:49:32 -0500

    Finish adding all suggested code fragments

--- app/src/main/java/ca/mcgill/ecse321/eventregistration/MainActivity.java
+++ app/src/main/java/ca/mcgill/ecse321/eventregistration/MainActivity.java
@@ -9,14 +9,29 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Iterator;
 
 import ca.mcgill.ecse321.eventregistration.controller.EventRegistrationController;
 import ca.mcgill.ecse321.eventregistration.controller.InvalidInputException;
+import ca.mcgill.ecse321.eventregistration.model.Event;
+import ca.mcgill.ecse321.eventregistration.model.Participant;
+import ca.mcgill.ecse321.eventregistration.model.RegistrationManager;
 import ca.mcgill.ecse321.eventregistration.persistence.PersistenceEventRegistration;
 import ca.mcgill.ecse321.eventregistration.persistence.PersistenceXStream;
 
 public class MainActivity extends AppCompatActivity {
+
+    // data elements
+    private HashMap<Integer, Participant> participants;
+    private HashMap<Integer, Event> events;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,8 +57,48 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void refreshData() {
-        TextView tv = (TextView) findViewById(R.id.newparticipant_name);
-        tv.setText("");
+
+        RegistrationManager rm = RegistrationManager.getInstance();
+
+        TextView newParticipant = (TextView) findViewById(R.id.newparticipant_name);
+        newParticipant.setText("");
+
+        TextView newEvent = (TextView) findViewById(R.id.newevent_name);
+        newEvent.setText("");
+
+        // Initialize the data in the participant spinner
+        Spinner participantSpinner = (Spinner) findViewById(R.id.participantspinner);
+        ArrayAdapter<CharSequence> participantAdapter = new
+                ArrayAdapter<CharSequence>(this, android.R.layout.simple_spinner_item);
+        participantAdapter.setDropDownViewResource(
+                android.R.layout.simple_spinner_dropdown_item);
+        this.participants = new HashMap<Integer, Participant>();
+
+        int i = 0;
+        for (Iterator<Participant> participants = rm.getParticipants().iterator();
+                participants.hasNext(); i++) {
+            Participant p = participants.next();
+            participantAdapter.add(p.getName());
+            this.participants.put(i,p);
+        }
+        participantSpinner.setAdapter(participantAdapter);
+
+        // Initialize the data in the event spinner
+        Spinner eventSpinner = (Spinner) findViewById(R.id.eventspinner);
+        ArrayAdapter<CharSequence> eventAdapter = new
+                ArrayAdapter<CharSequence>(this, android.R.layout.simple_spinner_item);
+        eventAdapter.setDropDownViewResource(
+                android.R.layout.simple_spinner_dropdown_item);
+        this.events = new HashMap<Integer, Event>();
+
+        i = 0;
+        for (Iterator<Event> events = rm.getEvents().iterator();
+             events.hasNext(); i++) {
+            Event e = events.next();
+            eventAdapter.add(e.getName());
+            this.events.put(i,e);
+        }
+        eventSpinner.setAdapter(eventAdapter);
     }
 
 //    @Override
@@ -68,15 +123,102 @@ public class MainActivity extends AppCompatActivity {
 //        return super.onOptionsItemSelected(item);
 //    }
 
+
+    //TODO Register Button
+
+
     public void addParticipant(View v) {
-        TextView tv = (TextView) findViewById(R.id.newparticipant_name);
-        EventRegistrationController pc = new EventRegistrationController();
+        TextView newParticipant = (TextView) findViewById(R.id.newparticipant_name);
+        EventRegistrationController erc = new EventRegistrationController();
         try {
-            pc.createParticipant(tv.getText().toString());
+            erc.createParticipant(newParticipant.getText().toString());
         } catch (InvalidInputException e) {
             //TODO handle error
         }
 
         refreshData();
     }
+
+    public void showDatePickerDialog(View v) {
+        TextView tf = (TextView) v;
+        Bundle args = getDateFromLabel(tf.getText());
+        args.putInt("id", v.getId());
+
+        DatePickerFragment newFragment = new DatePickerFragment();
+        newFragment.setArguments(args);
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
+    public void setDate(int id, int day, int month, int year) {
+        TextView tv = (TextView) findViewById(id);
+        tv.setText(String.format("%02d-%02d-%04d", day, month + 1, year));
+    }
+
+    private Bundle getDateFromLabel(CharSequence text) {
+        Bundle rtn = new Bundle();
+        String comps[] = text.toString().split("-");
+        int day =1;
+        int month=1;
+        int year = 1;
+
+        if (comps.length == 3) {
+            day = Integer.parseInt(comps[0]);
+            month = Integer.parseInt(comps[1]);
+            year = Integer.parseInt(comps[2]);
+        }
+
+        rtn.putInt("day", day);
+        rtn.putInt("month", month - 1);
+        rtn.putInt("year", year);
+
+        return rtn;
+    }
+
+    public void showTimePickerDialog(View v) {
+        TextView tf = (TextView) v;
+        Bundle args = getTimeFromLabel(tf.getText());
+        args.putInt("id", v.getId());
+
+        TimePickerFragment newFragment = new TimePickerFragment();
+        newFragment.setArguments(args);
+        newFragment.show(getSupportFragmentManager(), "timePicker");
+    }
+
+    public void setTime(int id, int hourOfDay, int minute) {
+        TextView tv = (TextView) findViewById(id);
+        tv.setText(String.format("%02d:%02d", hourOfDay, minute));
+    }
+
+    private Bundle getTimeFromLabel(CharSequence text) {
+        Bundle rtn = new Bundle();
+        String comps[] = text.toString().split(":");
+        int hour = 12;
+        int minute = 0;
+
+        if (comps.length == 2) {
+            hour = Integer.parseInt(comps[0]);
+            minute = Integer.parseInt(comps[1]);
+        }
+
+        rtn.putInt("hour", hour);
+        rtn.putInt("minute", minute);
+
+        return rtn;
+    }
+
+    public void addEvent(View v) {
+
+        EventRegistrationController erc = new EventRegistrationController();
+
+        TextView eventName = (TextView) findViewById(R.id.newevent_name);
+        TextView eventDate = (TextView) findViewById(R.id.newevent_date);
+
+        Calendar calendar = Calendar.getInstance();
+        //calendar.setTime((Date) );
+
+        //TODO finish arguments
+        //erc.createEvent(eventName.getText(), );
+    }
+
+
 }
